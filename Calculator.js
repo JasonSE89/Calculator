c0d0b4ec3f2460d571bbb12054e06f7283b927fc
Jason <jason.epstein89@gmail.com>
2018-05-21 01:42:31 -0400

    Basic calculator code completed

--- Calculator.js
+++ Calculator.js
@@ -4,7 +4,17 @@ function events(){
   for(var i=0;i<inputs.length; i++)
   {
     inputs[i].addEventListener("click", function(){
+        if(this.value=="c")
+        {
+          document.getElementById('screen').innerHTML = "";
+        }
+        else if(this.value=="="){
+          var result = eval(document.getElementById('screen').innerHTML);
+          document.getElementById('screen').innerHTML = result;
+        }
+        else{
         document.getElementById('screen').innerHTML += this.value;
+      }
     });
   }
 }
